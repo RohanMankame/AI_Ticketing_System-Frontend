b1bf98f3aa23bfb1f4503179307295f88d16d655
Rohan Mankame <rohanmankame@outlook.com>
2026-02-04 14:10:39 +0530

    update styling calander

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -62,7 +62,7 @@ const CalendarPage = () => {
 
     return (
         <div className="h-full flex flex-col md:flex-row gap-6">
-            <div className="flex-1 bg-white dark:bg-gray-800 p-4 rounded-lg shadow ">
+            <div className="flex-1 bg-white dark:bg-gray-800 p-4 rounded-lg shadow "> 
                 <Calendar
                 localizer={localizer}
                 events={events}
